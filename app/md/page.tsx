2a1907198cefe64a14ef215caf6899b908aa0a76
Moussoukoro <moussousauvegarde1@gmail.com>
2024-11-02 18:46:01 +0100

    CE0941

--- app/md/page.tsx
+++ app/md/page.tsx
@@ -243,7 +243,7 @@ export default function ResetPassword() {
     setError('');
 
     try {
-      const response = await fetch(`https://backhotelmanage-2.onrender.com/api/auth/reset-password/${token}`, {
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL+`/api/auth/reset-password/${token}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
